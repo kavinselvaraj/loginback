b59f0beb7f7fc1691f820ef577daa41b6960c27b
Kavin Kumar S <kavingowthaman@gmail.com>
2019-04-26 08:50:33 +0530

    login

--- loginback/routes/signup.js
+++ loginback/routes/signup.js
@@ -3,21 +3,29 @@ var router = express.Router();
 /* GET users listing. */
 router.post('/add_user', function(req, res) {
    var data=req.body;
-   
    firstname=data.firstname
    lastname=data.lastname
    username=data.username
    password=data.password
-
-   console.log(data.firstname)
    var sqlquery="INSERT INTO user_register (firstName,lastName,userName,password) values('"+firstname+"','"+lastname+"','"+username+"','"+password+"')";
-  
    req.app.locals.connection.query(sqlquery,function(error,results,fields){
       if(error) throw error;
-      var output={status:parseInt(200),'statuscode':true,message:'Successfully Inserted'}
+      console.log(results)
+      var output={status:parseInt(200),'statuscode':true,message:'ok',result:results}
       res.send(output);
    })
-  
   });
-  
+
+  router.post('/login_user', function(req, res) {
+   var data=req.body;
+   username=data.username
+   password=data.password
+   var sqlquery="SELECT * FROM user_register where userName='"+username+"' and password='"+password+"'";
+   req.app.locals.connection.query(sqlquery,function(error,results,fields){
+      if(error) throw error;
+      console.log(results)
+      var output={status:parseInt(200),'statuscode':true,message:'ok',result:results}
+      res.send(output);
+   })
+  });
   module.exports = router;
